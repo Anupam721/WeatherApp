b266c3418c0ad39738a78d151ed78e224b95b24a
Prabhat Singh <prabhatsingh@Prabhats-MacBook-Air.local>
2021-07-02 21:32:28 +0530

    kahani ab tak

--- lib/library/api_request.dart
+++ lib/library/api_request.dart
@@ -1,10 +1,32 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
 
-void getHttp() async {
-  try {
-    var response = await Dio().get('http://www.google.com');
-    print(response);
-  } catch (e) {
-    print(e);
+import 'package:dio/dio.dart';
+import 'package:weather_app/library/utils.dart';
+import 'package:weather_app/models/Weather.dart';
+
+// void getHttp() async {
+//   try {
+//     var response = await Dio().get('http://www.google.com');
+//     print(response);
+//   } catch (e) {
+//     print(e);
+//   }
+// }
+
+// api.openweathermap.org/data/2.5/weather?lat=28.644800&lon=77.216721&appid=d2d4ad0dc2ef04c44fa5c63247642b2b
+
+Future<WeatherData> getData() async {
+  String url = 'https://api.openweathermap.org/data/2.5/weather?';
+  Map<String, dynamic> queryParams = {
+    'lat': 28.644800,
+    'lon': 77.216721,
+    'appid': 'd2d4ad0dc2ef04c44fa5c63247642b2b',
+    'units': 'metric'
+  };
+
+  Response response = await Dio().get(url, queryParameters: queryParams);
+  if (response.statusCode != 200 && response.statusCode != 201) {
+    print("Some Error occurred-${response.statusCode}");
   }
+  return WeatherData.fromJson(response.data);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/library/api_request.dart';
+import 'package:weather_app/models/Weather.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,44 +23,55 @@ class MyAppState extends State<MyApp> {
             image: DecorationImage(
                 image: AssetImage('assets/app_wallpaper.jpg'),
                 fit: BoxFit.fill)),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(padding: EdgeInsets.all(120)),
-              Text(
-                "Monaco",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Friday,",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                    ),
+        child: FutureBuilder<WeatherData>(
+            future: getData(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                DateTime date =
+                    DateTime.fromMicrosecondsSinceEpoch(snapshot.data!.dt);
+
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(padding: EdgeInsets.all(120)),
+                      Text(
+                        '${snapshot.data?.name}',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "${date.year},",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                            ),
+                          ),
+                          Text(
+                            '${date.month} ${date.day}',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                            ),
+                          )
+                        ],
+                      ),
+                      Text(
+                        '${snapshot.data?.main.temp} °C',
+                        style: TextStyle(color: Colors.white, fontSize: 70),
+                      )
+                    ],
                   ),
-                  Text(
-                    "September 25",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                    ),
-                  )
-                ],
-              ),
-              Text(
-                "23 °C",
-                style: TextStyle(color: Colors.white, fontSize: 70),
-              )
-            ],
-          ),
-        ),
+                );
+              } else {
+                return CircularProgressIndicator();
+              }
+            }),
       )),
     );
   }
